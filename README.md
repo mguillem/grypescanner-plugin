2fdfd31a07ecd7e1f1ca306b33f698474549750e
dtbaum <dim.tbaum@gmail.com>
2022-02-21 18:13:49 +0100

    added pipeline support & improved README.md

--- README.md
+++ README.md
@@ -2,12 +2,49 @@
 
 ## Introduction
 
-Jenkins integration of Grype vulnerability scanner for container images and filesystems, see https://github.com/anchore/grype.
+[Grype](https://github.com/anchore/grype) is a vulnerability scanner for container images and filesystems.
+This jenkins plugin scans a given target and saves a report as job artifact.
+
 
 ## Getting started
+This jenkins plugin installs grype in the job workspace directory and performs scan. 
+See section [https://github.com/anchore/grype](Installation/Recommended) for more details.
+ 
 
-TODO Tell users how to configure your plugin here, include screenshots, pipeline examples and 
-configuration-as-code examples.
+#### Grype as a build step:
+<img src="images/1.png" alt="Grype plugin" />
+<img src="images/2.png" alt="Grype plugin" />
+
+#### Possible scan targets:
+<img src="images/3.png" alt="Grype plugin" />
+
+#### Scan result as job artifact:
+<img src="images/4.png" alt="Grype plugin" />
+
+#### Scan results:
+<img src="images/5.png" alt="Grype plugin" />
+     
+
+###Usage in a pipeline:
+pipeline  
+{  
+&nbsp;agent any  
+&nbsp;&nbsp;options  
+&nbsp;{  
+&nbsp;&nbsp;skipStagesAfterUnstable()  
+&nbsp;}  
+&nbsp;stages  
+&nbsp;{  
+&nbsp;&nbsp;stage('Build')  
+&nbsp;&nbsp;{  
+&nbsp;&nbsp;&nbsp;steps  
+&nbsp;&nbsp;&nbsp;{  
+&nbsp;&nbsp;&nbsp;step([$class: 'GrypeScannerStep', scanDest: 'dir:/tmp', repName: 'myScanResult.txt'])  
+&nbsp;&nbsp;&nbsp;}  
+&nbsp;&nbsp;}  
+&nbsp;}  
+}  
+
 
 ## Issues
 
--- src/main/java/io/jenkins/plugins/grypescanner/GrypeScannerStep.java
+++ src/main/java/io/jenkins/plugins/grypescanner/GrypeScannerStep.java
@@ -2,124 +2,108 @@ package io.jenkins.plugins.grypescanner;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.commons.lang.SystemUtils;
-import org.kohsuke.stapler.AncestorInPath;
 import org.kohsuke.stapler.DataBoundConstructor;
-import org.kohsuke.stapler.QueryParameter;
 import org.kohsuke.stapler.StaplerRequest;
 
-import hudson.EnvVars;
 import hudson.Extension;
 import hudson.FilePath;
 import hudson.Launcher;
 import hudson.Util;
-import hudson.Launcher.LocalLauncher;
 import hudson.model.AbstractProject;
-import hudson.model.Descriptor;
 import hudson.model.Run;
 import hudson.model.TaskListener;
 import hudson.tasks.ArtifactArchiver;
 import hudson.tasks.BuildStepDescriptor;
-import hudson.tasks.BuildStepMonitor;
 import hudson.tasks.Builder;
-import hudson.util.FormValidation;
 import jenkins.tasks.SimpleBuildStep;
 import net.sf.json.JSONObject;
 
 public class GrypeScannerStep extends Builder implements SimpleBuildStep
 {
+  private static final String SCAN_TARGET_DEFAULT = "dir:/";
+  private static final String REP_NAME_DEFAULT = "grypeReport_${JOB_NAME}_${BUILD_NUMBER}.txt";
+  
+  private String scanDest;
+  private String repName;
 
   // Fields in config.jelly must match the parameter names in the "DataBoundConstructor"
   @DataBoundConstructor
-  public GrypeScannerStep()
+  public GrypeScannerStep(String scanDest, String repName)
   {
-    System.out.println("GrypeScannerStep()");
+    this.scanDest = scanDest;
+    this.repName = repName;
   }
 
   @Override
-  public Descriptor<Builder> getDescriptor()
-  {
-    System.out.println("getDescriptor");
-    return super.getDescriptor();
-  }
-
-  @Override
-  public void perform(Run<?, ?> run, FilePath workspace, EnvVars env, Launcher launcher, TaskListener listener)
+  public void perform(Run<?, ?> run, FilePath workspace, Launcher launcher, TaskListener listener)
       throws InterruptedException, IOException
   {
-    String SCAN_TARGET = "dir:/tmp";
+    String scanDestresolved = Util.replaceMacro(scanDest, run.getEnvironment(listener));
+    String repNameResolved = Util.replaceMacro(repName, run.getEnvironment(listener));
+    
     FilePath grypeInstallDir = workspace.child("grypeInstallDir");
     grypeInstallDir.mkdirs();
+    Map<String, String> env = new HashMap<>();
     env.put("GRYPE_DB_CACHE_DIR", grypeInstallDir.toURI().getPath());
 
     FilePath script = grypeInstallDir.child("install.sh");
     FilePath grypeBinary = grypeInstallDir.child("grype");
-    FilePath resultReport = grypeInstallDir.child("grypeReport.txt");
+    FilePath resultReport = grypeInstallDir.child(repNameResolved);
     FilePath templateFile = grypeInstallDir.child("default.tmpl");
     script.copyFrom(new URL("https://raw.githubusercontent.com/anchore/grype/main/install.sh"));
 
-    // launcher.launch().cmds("","").;
-    
-    launcher.launch().cmds("hostname").envs(env)
-        .stdout(listener).stderr(listener.getLogger()).pwd(workspace).join();
-
-    
-    
+    launcher.launch().cmds("hostname").envs(env).stdout(listener).stderr(listener.getLogger()).pwd(workspace).join();
 
     listener.getLogger().println(
-        "Installing grype on destination: sh " + script.toURI().getPath() + " -b " + grypeInstallDir.toURI().getPath());
+        "Installing grype on destination:");
     int ret = launcher.launch().cmds("sh", script.toURI().getPath(), "-b", grypeInstallDir.toURI().getPath()).envs(env)
         .stdout(listener).stderr(listener.getLogger()).pwd(workspace).join();
     listener.getLogger().println("return value: " + ret);
 
-    listener.getLogger().println("Updating grype database: grype db update");
+    listener.getLogger().println("Updating grype database:");
     ret = launcher.launch().cmds(grypeBinary.toURI().getPath(), "db", "update").envs(env).stdout(listener)
         .stderr(listener.getLogger()).pwd(grypeInstallDir).join();
     listener.getLogger().println("return value: " + ret);
 
     templateFile.copyFrom(GrypeScanner.class.getResource("/default.tmpl"));
-    listener.getLogger().println("Running: " + grypeBinary.toURI().getPath() + " " + SCAN_TARGET + " -o " + "template " +
-        "-t " + templateFile.toURI().getPath() + " --file " + resultReport.toURI().getPath());
+    listener.getLogger().println("Running grype scan: ");
     ret = launcher.launch()
-        .cmds(grypeBinary.toURI().getPath(), SCAN_TARGET, "-o", "template", "-t", templateFile.toURI().getPath(),
+        .cmds(grypeBinary.toURI().getPath(), scanDestresolved, "-o", "template", "-t", templateFile.toURI().getPath(),
             "--file", resultReport.toURI().getPath())
         .envs(env).stdout(listener).stderr(listener.getLogger()).pwd(grypeInstallDir).join();
     listener.getLogger().println("return value: " + ret);
 
-    ArtifactArchiver artifactArchiver = new ArtifactArchiver("grypeInstallDir/grypeReport.txt");
-    artifactArchiver.perform(run, workspace, env, launcher, listener);
-    
-//  TODO:   LocalLauncher localLauncher = new LocalLauncher(listener);
-
-    // GrypeScanner gs = new GrypeScanner(listener.getLogger(), workspace.absolutize().);
-    // GrypeScanner.downloadGrype();
-    //
-    // GrypeScanner.installGrype(listener.getLogger());
-    // GrypeScanner.updateGrypeDb(listener.getLogger());
-    // GrypeScanner.executeGrype(listener.getLogger());
-    //
-    // ArtifactArchiver artifactArchiver = new ArtifactArchiver("scanout*");
-
+    ArtifactArchiver artifactArchiver = new ArtifactArchiver("grypeInstallDir/" + repNameResolved);
+    artifactArchiver.perform(run, workspace, launcher, listener);
   }
 
-  @Override
-  public boolean requiresWorkspace()
+  public String getScanDest()
   {
-    return true;
+    return scanDest;
   }
 
-  @Override
-  public BuildStepMonitor getRequiredMonitorService()
+  public void setScanDest(String scanDest)
   {
-    System.out.println("getRequiredMonitorService");
-    return super.getRequiredMonitorService();
+    this.scanDest = scanDest;
+  }
+
+  public String getRepName()
+  {
+    return repName;
+  }
+
+  public void setRepName(String repName)
+  {
+    this.repName = repName;
   }
 
   @Extension(ordinal = -2)
   public static final class DescriptorImpl extends BuildStepDescriptor<Builder>
   {
-    private static final String DEFAULT_VNS_SERV = "localhost:5900";
 
     public DescriptorImpl()
     {
@@ -130,30 +114,26 @@ public class GrypeScannerStep extends Builder implements SimpleBuildStep
     @Override
     public boolean configure(StaplerRequest req, JSONObject json) throws FormException
     {
-      System.out.println("configureeeeeeeeeeeeeeeeeeeeeee");
       req.bindJSON(this, json);
       save();
       return true;
     }
 
-    public FormValidation doCheckVncServ(@AncestorInPath AbstractProject<?, ?> project, @QueryParameter String value)
+    
+    public String getDefaultScanDest()
     {
-      System.out.println("doCheckVncServ");
-      return FormValidation
-          .okWithMarkup("<strong><font color=\"blue\">Please, make sure that your vncserver is running on '"
-              + Util.xmlEscape(value) + "'</font></strong>");
+      return SCAN_TARGET_DEFAULT;
     }
 
     @Override
     public String getDisplayName()
     {
-      System.out.println("getDisplayName");
-      return "Enable scannerrr";
+      return "Vulnerability scan with grype";
     }
 
-    public String getDefaultVncServ()
+    public String getDefaultRepName()
     {
-      return DEFAULT_VNS_SERV;
+      return REP_NAME_DEFAULT;
     }
 
     @Override
@@ -162,5 +142,4 @@ public class GrypeScannerStep extends Builder implements SimpleBuildStep
       return !SystemUtils.IS_OS_WINDOWS;
     }
   }
-
 }
